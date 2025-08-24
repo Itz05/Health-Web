18ff026e3765d989ea464f80d201d97fda7123a0
Juan Cornejo <arleyjuan00@gmail.com>
2025-08-24 09:22:40 -0500

    Updates

--- firebase.js
+++
@@ -1,8 +0,0 @@
-import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../firebase';
-
-export const fetchDocumentById = async (collection, id) => {
-  const ref = doc(db, collection, id);
-  const snapshot = await getDoc(ref);
-  return snapshot.exists() ? snapshot.data() : null;
-};
